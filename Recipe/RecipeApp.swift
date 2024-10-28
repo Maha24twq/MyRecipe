e3c8076b86863a9dc191fe8d40102e189812fc5d
Maha24twq <mahasalem09.09.0@gmail.com>
2024-10-28 15:43:46 +0300

    Initial Commit

---
+++ Recipe/RecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeApp.swift
+//  Recipe
+//
+//  Created by Maha Salem Alghmadi on 25/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct RecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
